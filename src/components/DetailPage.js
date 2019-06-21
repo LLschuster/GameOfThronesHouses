d5b67446ca59ccf3cae42bac50b4e33ab86e4176
llschuster <perez99leo@outlook.es>
2019-06-21 11:48:51 +0200

    options for more information in details view added

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,17 +1,24 @@
 import React, { useState, useEffect} from 'react'
 import {fetchUrl} from '../utils/constants'
 import {FetchData} from '../utils/functions'
+import { async } from 'q';
+import ExtraInfo from './ExtraInfo';
 
  const DetailPage = (props) =>{
     const [houseInfo, setHouseInfo] = useState([]);
+    const [extraInfo, setExtraInfo] = useState([]);
     
-    async function GetHouseData(url, houseIndex=null){
+    async function GetHouseData(url, houseIndex){
      let house = await FetchData(url, houseIndex);
      return house
     }
 
-    function handleExtraData(url){
+   async function handleExtraData(url){
         console.log(url);
+        let extra = await FetchData(url).then(data=>{
+            console.log(data.name);
+            setExtraInfo(data.name)
+        });
     }
     useEffect(()=>{
         let houseIndex = props.match.params.house;
@@ -27,15 +34,20 @@ import {FetchData} from '../utils/functions'
                 houseInfo && Object.entries(houseInfo).map(([key, value]) => {
                     if (key==='name')
                         return (<h2>Details of {value}</h2>)
-                    if (key==='url' || value=='')
+                    if (key==='url' || value=='' || key==='swornMembers')
                         return;
                     if (key==='overlord')
+                        return (<button onClick={()=>handleExtraData(value)}>show {key}</button>)
+                    if (key==='currentLord')
+                        return (<button onClick={()=>handleExtraData(value)}>show {key}</button>)
+                    if (key==='heir')
                         return (<button onClick={()=>handleExtraData(value)}>show {key}</button>)
                     return (
                         <h4 className='infoItem'>{key} : {value}</h4>
                     )
                 })
             }
+            <ExtraInfo info={extraInfo}/>
             </div>
         )
     }
--- src/components/ExtraInfo.js
+++ src/components/ExtraInfo.js
@@ -1,7 +1,12 @@
+import React from 'react'
 
 
 const ExtraInfo = (props) => {
-
+    return (
+        <>
+            <p>{props && props.info}</p>
+        </>
+    )
 }
 
 export default ExtraInfo
